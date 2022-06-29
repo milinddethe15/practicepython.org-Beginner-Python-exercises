a6c06af5dd30bb827e77e87b352f62cbffee0b20
milinddethe15 <99114125+milinddethe15@users.noreply.github.com>
2022-06-29 23:04:43 +0530

    Add files via upload

---
+++ Ex2.py
@@ -0,0 +1,11 @@
+x=int(input("enter a number:"))
+if x%2==0:
+   print("entered number is even")
+else : 
+    print("entered number is odd")
+if x%4==0:
+    print("entered number is divisible by 4")
+num=int(input("enter num:"))
+check=int(input("enter check:"))
+if (num/check)%2==0:
+    print("check divides evenly into num")
